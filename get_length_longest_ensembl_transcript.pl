ecd0abd9021a45e23b468269bc61c18dfad8245e
Ian Sealy <ian.sealy@sanger.ac.uk>
2014-07-03 15:07:27 +0100

    Script to get the length of the longest transcript of each Ensembl gene.

---
+++ get_length_longest_ensembl_transcript.pl
@@ -0,0 +1,197 @@
+#!/usr/bin/env perl
+
+# PODNAME: get_length_longest_ensembl_transcript.pl
+# ABSTRACT: Get the length of the longest transcript of each Ensembl gene
+
+## Author     : ian.sealy@sanger.ac.uk
+## Maintainer : ian.sealy@sanger.ac.uk
+## Created    : 2014-07-03
+
+use warnings;
+use strict;
+use autodie;
+use Getopt::Long;
+use Pod::Usage;
+use Carp;
+use version; our $VERSION = qv('v0.1.0');
+
+use Bio::EnsEMBL::Registry;
+use Bio::Seq;
+use Bio::SeqIO;
+
+# Default options
+my $species        = 'Danio rerio';
+my $ensembl_dbhost = 'ensembldb.ensembl.org';
+my $ensembl_dbport;
+my $ensembl_dbuser = 'anonymous';
+my $ensembl_dbpass;
+my ( $debug, $help, $man );
+
+# Get and check command line options
+get_and_check_options();
+
+# Connnect to Ensembl database
+Bio::EnsEMBL::Registry->load_registry_from_db(
+    -host => $ensembl_dbhost,
+    -port => $ensembl_dbport,
+    -user => $ensembl_dbuser,
+    -pass => $ensembl_dbpass,
+);
+
+# Get genebuild version
+my $genebuild_version = 'e' . Bio::EnsEMBL::ApiVersion::software_version();
+warn 'Genebuild version: ', $genebuild_version, "\n" if $debug;
+
+# Get Ensembl adaptors
+my $ga = Bio::EnsEMBL::Registry->get_adaptor( $species, 'core', 'Gene' );
+my $sa = Bio::EnsEMBL::Registry->get_adaptor( $species, 'core', 'Slice' );
+
+# Ensure database connection isn't lost; Ensembl 64+ can do this more elegantly
+## no critic (ProhibitMagicNumbers)
+if ( Bio::EnsEMBL::ApiVersion::software_version() < 64 ) {
+## use critic
+    Bio::EnsEMBL::Registry->set_disconnect_when_inactive();
+}
+else {
+    Bio::EnsEMBL::Registry->set_reconnect_when_lost();
+}
+
+# Get all genes and their transcripts
+my $genes = $ga->fetch_all();
+foreach my $gene ( @{$genes} ) {
+    my $max_length  = 0;
+    my $transcripts = $gene->get_all_Transcripts();
+    foreach my $transcript ( @{$transcripts} ) {
+        if ( $transcript->length > $max_length ) {
+            $max_length = $transcript->length;
+        }
+    }
+    printf "%s\t%d\n", $gene->stable_id, $max_length;
+    $gene->flush_Transcripts();    # Save memory
+}
+
+# Get and check command line options
+sub get_and_check_options {
+
+    # Get options
+    GetOptions(
+        'species=s'        => \$species,
+        'ensembl_dbhost=s' => \$ensembl_dbhost,
+        'ensembl_dbport=i' => \$ensembl_dbport,
+        'ensembl_dbuser=s' => \$ensembl_dbuser,
+        'ensembl_dbpass=s' => \$ensembl_dbpass,
+        'debug'            => \$debug,
+        'help'             => \$help,
+        'man'              => \$man,
+    ) or pod2usage(2);
+
+    # Documentation
+    if ($help) {
+        pod2usage(1);
+    }
+    elsif ($man) {
+        pod2usage( -verbose => 2 );
+    }
+
+    return;
+}
+
+__END__
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+get_length_longest_ensembl_transcript.pl
+
+Get the length of the longest transcript of each Ensembl gene
+
+=head1 VERSION
+
+version 0.1.0
+
+=head1 DESCRIPTION
+
+This script dumps a list of Ensembl gene stable IDs along with the length of the
+longest transcript.
+
+=head1 EXAMPLES
+
+    perl \
+        -Ibranch-ensembl-75/ensembl/modules \
+        get_length_longest_ensembl_transcript.pl
+
+=head1 USAGE
+
+    get_length_longest_ensembl_transcript.pl
+        [--species species]
+        [--ensembl_dbhost host]
+        [--ensembl_dbport port]
+        [--ensembl_dbuser username]
+        [--ensembl_dbpass password]
+        [--debug]
+        [--help]
+        [--man]
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--species SPECIES>
+
+Species (defaults to Danio rerio).
+
+=item B<--ensembl_dbhost HOST>
+
+Ensembl MySQL database host.
+
+=item B<--ensembl_dbport PORT>
+
+Ensembl MySQL database port.
+
+=item B<--ensembl_dbuser USERNAME>
+
+Ensembl MySQL database username.
+
+=item B<--ensembl_dbpass PASSWORD>
+
+Ensembl MySQL database password.
+
+=item B<--debug>
+
+Print debugging information.
+
+=item B<--help>
+
+Print a brief help message and exit.
+
+=item B<--man>
+
+Print this script's manual page and exit.
+
+=back
+
+=head1 DEPENDENCIES
+
+Ensembl Perl API - http://www.ensembl.org/info/docs/api/
+
+=head1 AUTHOR
+
+=over 4
+
+=item *
+
+Ian Sealy <ian.sealy@sanger.ac.uk>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2014 by Genome Research Ltd.
+
+This is free software, licensed under:
+
+  The GNU General Public License, Version 3, June 2007
+
+=cut
