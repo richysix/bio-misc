fa97fcb4ebc68e69a8f6a284649442862f9dbd4d
Ian Sealy <ian.sealy@sanger.ac.uk>
2015-03-04 11:35:53 +0000

    Script to graph RNA-Seq count data.

---
+++ graph_rnaseq_counts.R
@@ -0,0 +1,42 @@
+#!/usr/bin/env Rscript
+
+# Graph count data from RNA-Seq output produced by:
+# https://gist.github.com/iansealy/2dca28d07c0764e014df
+# or https://gist.github.com/iansealy/b9cbc56bd1affe10d37a
+
+Args        <- commandArgs()
+dataFile    <- ifelse(is.na(Args[6]), "all.tsv",            Args[6])
+samplesFile <- ifelse(is.na(Args[7]), "deseq2/samples.txt", Args[7])
+pdfFile     <- ifelse(is.na(Args[8]), "counts.pdf",         Args[8])
+
+# Read data
+data <- read.delim(dataFile, header=TRUE)
+
+# Read samples
+samples <- read.table( samplesFile, header=TRUE, row.names=1 )
+
+# Graph parameters
+labels <- gsub(".normalised.count$", "",
+               names(data)[grepl(".normalised.count$", names(data))])
+colours <- as.numeric(samples[labels, "condition"])
+
+pdf(pdfFile)
+
+# Plot each region separately
+for (i in 1:nrow(data)) {
+    counts <- data[i, grepl(".normalised.count$", names(data)) ]
+    par(mar=c(8.1, 4.1, 4.1, 2.1), xpd=TRUE)
+    plot(as.numeric(counts), axes=FALSE, ann=FALSE, pch=21, bg=colours)
+    axis(1, at=1:length(labels), lab=labels, las=2, cex.axis=0.5)
+    axis(2)
+    title(main=sprintf("%s:%d-%d\n%s\n%.2f",
+                       data[i,"Chr"], data[i,"Start"],
+                       data[i,"End"], data[i,"Gene.ID"],
+                       data[i,"adjp"]))
+    title(xlab="")
+    title(ylab="Normalised Counts")
+    legend("topright", inset=c(0, -0.1), levels(samples$condition), pch=21,
+           pt.bg=1:length(levels(samples$condition)))
+}
+
+graphics.off()
