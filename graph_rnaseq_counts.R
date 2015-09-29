bf81c6ff80df16f9db46ff31ccfb12f1e430a3fd
Ian Sealy <ian.sealy@sanger.ac.uk>
2015-09-29 15:59:10 +0100

    Show gene names in count graphs.

--- graph_rnaseq_counts.R
+++ graph_rnaseq_counts.R
@@ -36,10 +36,10 @@ for (i in 1:nrow(data)) {
     plot(as.numeric(counts), axes=FALSE, ann=FALSE, pch=21, bg=colours)
     axis(1, at=1:length(labels), lab=labels, las=2, cex.axis=0.5)
     axis(2)
-    title(main=sprintf("%s:%d-%d\n%s\n%.2f",
+    title(main=sprintf("%s:%d-%d\n%s / %s\n%.2f",
                        data[i,"Chr"], data[i,"Start"],
                        data[i,"End"], data[i,"Gene.ID"],
-                       data[i,"adjp"]))
+                       data[i,"Name"], data[i,"adjp"]))
     title(xlab="")
     title(ylab="Normalised Counts")
     legend("topright", inset=c(0, -0.1), levels(samples$condition), pch=21,
