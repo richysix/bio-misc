62679717089388f27ff08ec2fb42beee40d464f4
Ian Sealy <git@iansealy.com>
2016-02-19 16:26:45 +0000

    Script to estimate number of clusters (and clusters) using hopach.

---
+++ estimate_clusters_rnaseq.R
@@ -0,0 +1,54 @@
+#!/usr/bin/env Rscript
+
+# Estimate clusters using RNA-Seq output produced by:
+# https://gist.github.com/iansealy/2dca28d07c0764e014df
+# or https://gist.github.com/iansealy/b9cbc56bd1affe10d37a
+
+suppressPackageStartupMessages(library(hopach))
+
+Args        <- commandArgs()
+dataFile    <- ifelse(is.na(Args[6]), "all.tsv",            Args[6])
+samplesFile <- ifelse(is.na(Args[7]), "deseq2/samples.txt", Args[7])
+outputBase  <- ifelse(is.na(Args[8]), "all",                Args[8])
+
+# Read data
+data <- read.delim(dataFile, header=TRUE, check.names=FALSE)
+
+# Support different column names
+names(data)[names(data) == 'chr']     <- 'Chr'
+names(data)[names(data) == 'start']   <- 'Start'
+names(data)[names(data) == 'end']     <- 'End'
+names(data)[names(data) == 'ID']      <- 'Gene.ID'
+names(data)[names(data) == 'adjpval'] <- 'adjp'
+
+# Read samples
+samples <- read.table( samplesFile, header=TRUE, row.names=1 )
+
+# Ensure chromosome is a factor, even if all numeric
+data[,1] <- factor(data[,1])
+
+# Get counts
+countData <- data[,grepl(".normalised.counts?$", names(data))]
+names(countData) <- gsub(".normalised.counts?$", "", names(countData))
+
+# Get median counts
+medianData <- matrix(nrow=nrow(countData),
+                     ncol=length(levels(samples$condition)),
+                     dimnames=list(rownames(countData),
+                                   levels(samples$condition)))
+for (condition in levels(samples$condition)) {
+    medianData[,condition] <-
+        apply(countData[,samples$condition == condition, drop=FALSE], 1,
+              median)
+}
+
+# Cluster
+distMatrix <- distancematrix(medianData, 'cosangle')
+hobj <- hopach(medianData, dmat=distMatrix)
+
+# Output clusters and number of clusters
+write.table(hobj$clust$k, file=paste0(outputBase, '-num-clusters.tsv'),
+            quote=FALSE, sep='\t', row.names=FALSE, col.names=FALSE)
+df <- data.frame(ID=data[,1], cluster=hobj$clust$labels)
+write.table(df, file=paste0(outputBase, '-clusters.tsv'),
+            quote=FALSE, sep='\t', row.names=FALSE, col.names=TRUE)
