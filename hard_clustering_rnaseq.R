31d41ab375ca71804a6f2871d176978f65f0d80c
Ian Sealy <git@iansealy.com>
2016-03-03 21:09:46 +0000

    Dump each cluster as table and counts plot.

--- hard_clustering_rnaseq.R
+++ hard_clustering_rnaseq.R
@@ -56,3 +56,31 @@ options(scipen=0)
 data$cluster <- clusters
 write.table(data, file=paste0(outputBase, '-clusters.tsv'),
             quote=FALSE, sep='\t', row.names=FALSE, col.names=TRUE)
+labels <- gsub(".normalised.*$", "",
+               names(data)[grepl(".normalised.*$", names(data))])
+colours <- as.numeric(samples$condition)
+for ( cluster in unique(sort(data$cluster)) ) {
+    data.subset <- data[data$cluster == cluster,]
+    write.table(data.subset, file=paste0(outputBase, '-cluster-', cluster,
+                                         '.tsv'),
+                quote=FALSE, sep='\t', row.names=FALSE, col.names=TRUE)
+    # Plot counts
+    pdf(paste0(outputBase, '-cluster-', cluster,
+               '-counts.pdf'))
+    for (i in 1:nrow(data.subset)) {
+        counts <- data.subset[i, grepl(".normalised.*$", names(data.subset)) ]
+        par(mar=c(8.1, 4.1, 4.1, 2.1), xpd=TRUE)
+        plot(as.numeric(counts), axes=FALSE, ann=FALSE, pch=21, bg=colours)
+        axis(1, at=1:length(labels), lab=labels, las=2, cex.axis=0.5)
+        axis(2)
+        title(main=sprintf("%s:%d-%d\n%s / %s\n%.2f",
+                           data.subset[i,"Chr"], data.subset[i,"Start"],
+                           data.subset[i,"End"], data.subset[i,"Gene.ID"],
+                           data.subset[i,"Name"], data.subset[i,"adjp"]))
+        title(xlab="")
+        title(ylab="Normalised Counts")
+        legend("topright", inset=c(0, -0.1), levels(samples$condition), pch=21,
+               pt.bg=1:length(levels(samples$condition)))
+    }
+    graphics.off()
+}
