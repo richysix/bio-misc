6ad91ae860e36b71b668519ac1a0f49b8085b892
Ian Sealy <git@iansealy.com>
2017-05-29 10:48:43 +0100

    Allow adding circle round gene of interest.

--- volcano_plot_rnaseq.R
+++ volcano_plot_rnaseq.R
@@ -5,17 +5,22 @@
 # or https://gist.github.com/iansealy/b9cbc56bd1affe10d37a
 
 Args        <- commandArgs()
-dataFile    <- ifelse(is.na(Args[6]),  "all.tsv",     Args[6])
-outputFile  <- ifelse(is.na(Args[7]),  "volcano.png", Args[7])
-maxLog2Fc   <- ifelse(is.na(Args[8]),  NA,            as.numeric(Args[8]))
-maxLogPVal  <- ifelse(is.na(Args[9]),  NA,            as.numeric(Args[9]))
-pValLine    <- ifelse(is.na(Args[10]), NA,            as.numeric(Args[10]))
+dataFile    <- ifelse(is.na(Args[6]), "all.tsv",     Args[6])
+outputFile  <- ifelse(is.na(Args[7]), "volcano.png", Args[7])
+maxLog2Fc   <- suppressWarnings(ifelse(is.na(Args[8]), NA,
+                                as.numeric(Args[8])))
+maxLogPVal  <- suppressWarnings(ifelse(is.na(Args[9]), NA,
+                                as.numeric(Args[9])))
+pValLine    <- suppressWarnings(ifelse(is.na(Args[10]), NA,
+                                as.numeric(Args[10])))
+markedGene  <- ifelse(is.na(Args[11]), NA, Args[11])
 
 # Read data
 data <- read.delim(dataFile, header=TRUE, check.names=FALSE)
 
 # Support different column names
 names(data)[names(data) == 'adjpval'] <- 'adjp'
+names(data)[names(data) == 'Gene ID'] <- 'GeneID'
 
 # Plot (red if adjp < 0.05; orange if log2fc > 1; green if both)
 png(outputFile)
@@ -38,6 +43,10 @@ with(subset(data, abs(log2fc) > 1), points(log2fc, -log10(adjp), pch=20,
                                            col="orange"))
 with(subset(data, adjp < 0.05 & abs(log2fc) > 1), points(log2fc, -log10(adjp),
                                                          pch=20, col="green"))
+if ( !is.na(markedGene) ) {
+    with(subset(data, GeneID == markedGene), points(log2fc, -log10(adjp),
+                                                    pch=21, col="blue", cex=2))
+}
 if ( !is.na(pValLine) ) {
     abline(h=-log10(pValLine), col="gray60", lty=2)
 }
